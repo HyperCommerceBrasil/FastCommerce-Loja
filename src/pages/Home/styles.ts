4adb083e2fef3328302d28bb17062558b3367a28
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-17 01:30:17 -0300

    removed comments

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { Colors } from '../../utils';
 
-// const { insane } = Spacings;
-
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
