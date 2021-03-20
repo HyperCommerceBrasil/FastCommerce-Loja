831d6a535b1b3a64b3a92678001b7e5b9ed32e0c
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-20 11:33:38 -0300

    improved content container

--- src/components/ContentWrapper/styles.ts
+++ src/components/ContentWrapper/styles.ts
@@ -2,7 +2,5 @@ import styled from 'styled-components';
 import { Colors } from '../../utils';
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
   background-color: ${Colors.light?.environment.lighter};
 `;
