27fe453e4132d870520f213c3a2dd93a94b931de
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-21 12:31:32 -0300

    feat(utils): created validators

---
+++ src/utils/validators/isValidEmail.ts
@@ -0,0 +1,7 @@
+const isValidEmail = (email: string): boolean => {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+    email,
+  );
+};
+
+export default isValidEmail;
