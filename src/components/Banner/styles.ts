276397da470d88ba5442885a765d41ae245355b2
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-21 14:29:16 -0300

    updated banner styles

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,40 +1,54 @@
 import styled from 'styled-components';
 import WhiteBackground from '../../assets/img/fundo-branco-elegante-com-linhas-brilhantes_1017-17580.png';
-import { Colors, Spacings } from '../../utils';
+import { Colors, Spacings, Breakpoints } from '../../utils';
 
-const { insane } = Spacings;
+const { laptop, tablet } = Breakpoints;
+const { insane, medium } = Spacings;
 
 export const Wrapper = styled.div`
-  /* background: linear-gradient(
-    135deg,
-    ${Colors.light?.environment.darker} 0%,
-    ${Colors.light?.primary.main} 100%
-  ); */
-  /* background-color: #f16; */
   background: url(${WhiteBackground});
   background-repeat: no-repeat;
   background-size: cover;
+
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-  flex-wrap: wrap-reverse;
   align-items: center;
+
+  @media (max-width: ${laptop}px) {
+    flex-direction: column-reverse;
+  }
 `;
 
-export const BuyerWrapper = styled.div`
-  margin: 0 ${insane}px;
-`;
+export const BuyerWrapper = styled.div``;
 
 export const BuyerOption = styled.div`
+  display: flex;
   flex-direction: column;
   margin: ${insane}px;
   padding-bottom: ${2 * insane}px;
+
+  @media (max-width: ${laptop}px) {
+    align-items: center;
+    padding-bottom: ${insane}px;
+    margin: ${insane}px ${medium}px;
+  }
 `;
 
 export const Title = styled.h2`
+  color: ${Colors.light?.text.darker};
   font-weight: 500;
   font-size: 2.4rem;
   margin-bottom: ${insane}px;
+
+  @media (max-width: ${laptop}px) {
+    text-align: center;
+    font-size: 2rem;
+  }
+
+  @media (max-width: ${tablet}px) {
+    font-size: 1.8rem;
+  }
 `;
 
 export const ImageWrapper = styled.div`
