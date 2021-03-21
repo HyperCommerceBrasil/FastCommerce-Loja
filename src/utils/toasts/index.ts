bb27a7a48a8e5ea840e7c21a6596d4c40856b527
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-21 12:34:29 -0300

    created toasts

---
+++ src/utils/toasts/index.ts
@@ -0,0 +1,2 @@
+export { default as success } from './success';
+export { default as error } from './error';
---
+++ src/utils/toasts/success.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+import { Colors, Spacings } from '..';
+
+const { small } = Spacings;
+
+export const success = (content: string | JSX.Element | React.FC): void => {
+  toast(content, {
+    type: 'success',
+    style: {
+      backgroundColor: Colors.light?.success,
+      color: Colors.light?.text.darker,
+      margin: small,
+    },
+    bodyStyle: { backgroundColor: Colors.light?.success },
+  });
+};
+export default success;
