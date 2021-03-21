dbd8c16f39774853d28c4c28be6f2a9292d16695
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-21 14:33:23 -0300

    organized public and private pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,7 @@
+// Public Pages
 export { default as Home } from './Home';
 export { default as ProductSearch } from './ProductSearch';
 export { default as SignUp } from './SignUp';
 export { default as DetailedProduct } from './DetailedProduct';
+
+// Private Pages
