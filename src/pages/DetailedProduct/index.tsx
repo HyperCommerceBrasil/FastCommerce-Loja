e7a58c1554f7591df6af17a08d946823076a169b
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-28 21:09:24 -0300

    updated product details page

--- src/pages/DetailedProduct/index.tsx
+++ src/pages/DetailedProduct/index.tsx
@@ -15,7 +15,7 @@ const DetailedProduct: React.FC = () => {
       <Header />
       <Categories />
       <ContentWrapper>
-        <ProductDetails productImage="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1604021663000" />
+        <ProductDetails />
       </ContentWrapper>
       <Footer shouldHaveBottomPadding={100} />
       <PurchaseBottom />
