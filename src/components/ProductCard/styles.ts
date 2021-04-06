8985a287b446c4543478ac7ffc19815fdea344a8
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-04-06 00:32:40 -0300

    improved product card

--- src/components/ProductCard/styles.ts
+++ src/components/ProductCard/styles.ts
@@ -21,6 +21,7 @@ export const Wrapper = styled.div`
   width: 18vw;
   height: 300px;
   box-shadow: 0 0 24px #cccccc;
+  transition: 0.4s;
   &:hover {
     cursor: pointer;
     box-shadow: 0 0 14px ${Colors.light?.primary.lighter};
