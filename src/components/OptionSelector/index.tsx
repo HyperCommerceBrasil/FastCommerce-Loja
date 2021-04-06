bda6cd9de1cfaf7939a9071042699e8f17696e87
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-04-06 00:32:20 -0300

    improved option selector

--- src/components/OptionSelector/index.tsx
+++ src/components/OptionSelector/index.tsx
@@ -1,11 +1,17 @@
-import React, { useContext, useState } from 'react';
-import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
-import { Wrapper } from './styles';
+import React, { useContext, useEffect, useState } from 'react';
+import { FormControl, InputLabel, MenuItem } from '@material-ui/core';
+import { useParams } from 'react-router';
+import { Wrapper, Select } from './styles';
 import { GlobalCategoriesContext } from '../../contexts';
 
-const OptionSelector: React.FC = () => {
+type OptionSelectorProps = {
+  query?: string;
+};
+
+const OptionSelector: React.FC<OptionSelectorProps> = () => {
+  const { query = 'Coleção Padrão' } = useParams<OptionSelectorProps>();
   const { categories } = useContext(GlobalCategoriesContext);
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState(query);
 
   const handleChange = (
     target: EventTarget & { name?: string | undefined; value: unknown },
@@ -13,9 +19,14 @@ const OptionSelector: React.FC = () => {
     const { value } = target;
     setCategory(value as string);
   };
+
+  useEffect(() => {
+    setCategory(query);
+  }, [query]);
+
   return (
     <Wrapper>
-      <FormControl variant="outlined" className="some_class_name">
+      <FormControl fullWidth variant="outlined">
         <InputLabel id="demo-simple-select-label">Categoria</InputLabel>
         <Select
           placeholder="Categoria"
