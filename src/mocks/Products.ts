478b47e7465ca0a6013d9217a0187cc7f90e5b5c
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-04-17 22:21:00 -0300

    added new mocks

---
+++ src/mocks/Products.ts
@@ -0,0 +1,462 @@
+export const Products: Product[] = [
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3613',
+      name: 'Widgets',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+  {
+    collection: {
+      id: 'fqe4tq3',
+      name: 'Coleção Padrão',
+    },
+    id: 'rt134rt134t1',
+    images: [
+      {
+        id: 'df34f43',
+        image:
+          'https://www.cbacessorios.com.br/media/catalog/product/cache/1/image/a4e40ebdc3e371adff845072e1c73f37/k/2/k2924.jpg',
+      },
+    ],
+    name: 'produto fone',
+    price: 371.71,
+    trending: true,
+    description: 'here is some description',
+    details: 'there are a lot of details',
+    quantity: 3,
+  },
+];
