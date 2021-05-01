87d987b5ab5a2c3202756d8c912c14a76a853099
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-05-01 17:22:21 -0300

    upgraded cart item

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IoMdClose } from 'react-icons/io';
 import { toLocalCurrency } from '../../utils';
 import {
   DetailsWrapper,
@@ -7,22 +8,38 @@ import {
   Price,
   TotalPrice,
   Wrapper,
+  HeaderCartItem,
+  DeleteWrapper,
+  TextDeatilsWrapper,
 } from './styles';
 
 type CartIconProps = {
   product: ProductOnCart;
+  handleRemoveProduct?(product: ProductOnCart): void;
 };
-const CartItem: React.FC<CartIconProps> = ({ product }) => {
-  return (
-    <Wrapper to={`/product/${product.id}`}>
+const CartItem: React.FC<CartIconProps> = ({
+  product,
+  handleRemoveProduct = () => ({}),
+}) => (
+  <Wrapper to={`/product/${product.id}`}>
+    <DetailsWrapper>
       <ProductImage src={product?.images[0].image} />
-      <DetailsWrapper>
-        <Name>{product?.name || 'Some name'}</Name>
-        <Price>{toLocalCurrency(product?.price || 551.61)}</Price>
-      </DetailsWrapper>
-      <TotalPrice>{toLocalCurrency(product?.price) || 'R$551.51'}</TotalPrice>
-    </Wrapper>
-  );
-};
+      <TextDeatilsWrapper>
+        <HeaderCartItem>
+          <Name>{product?.name || 'Some name'}</Name>
+        </HeaderCartItem>
+        <Price>{toLocalCurrency(product?.price)}</Price>
+        <TotalPrice>{toLocalCurrency(product?.price)}</TotalPrice>
+      </TextDeatilsWrapper>
+    </DetailsWrapper>
+    <DeleteWrapper>
+      <IoMdClose
+        color="#fff"
+        size={30}
+        onClick={() => handleRemoveProduct(product)}
+      />
+    </DeleteWrapper>
+  </Wrapper>
+);
 
 export default CartItem;
