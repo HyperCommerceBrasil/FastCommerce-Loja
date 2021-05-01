9539d1a787188cfa756e06226c43188756040576
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-05-01 17:23:44 -0300

    testing 404 routing

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -7,6 +7,19 @@ import {
   ProductSearchContainer,
 } from '../../pages';
 
+const What = () => (
+  <div
+    style={{
+      display: 'flex',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <h1>404</h1>
+  </div>
+);
+
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
@@ -14,6 +27,7 @@ const Routes: React.FC = () => (
       <Route path="/signup" component={SignUp} />
       <Route path="/product/:id" component={DetailedProduct} />
       <Route path="/search" component={ProductSearchContainer} />
+      <Route component={What} />
     </Switch>
   </BrowserRouter>
 );
