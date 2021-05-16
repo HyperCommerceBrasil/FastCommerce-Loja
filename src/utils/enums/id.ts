0ae3b5cbe40a3c2b9f12328523e26d4f9b577665
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-05-16 03:34:39 -0300

    fixed enum docs

--- src/utils/enums/id.ts
+++ src/utils/enums/id.ts
@@ -1,6 +1,6 @@
 /**
  * Use this to give the component's or tags's ids.
- * Let's follow the pattern CONTEXT_DESCRIPTION_OF_ID.
+ * Let's follow the pattern CONTEXT + DESCRIPTION_OF_ID.
  * If there is a list or a large amount where is possible to two or more same
  * ids, you can concantenate a more specific description BUT, using this enum
  * in the start.
