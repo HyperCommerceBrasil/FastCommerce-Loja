5d06ab6451282386e89f71a0819d28fe746522bb
Gabao-Farias-Builders <gabriel.farias@platformbuilders.io>
2021-07-18 17:17:54 -0300

    upgraded user api

--- src/services/public/user.ts
+++ src/services/public/user.ts
@@ -1,26 +1,51 @@
 import { AxiosRequestConfig } from 'axios';
+import ResponseErrorHandler from '../../utils/api/error';
 import api from '../api';
 
-// const baseURLPublic = '/public';
+const requestConfig: AxiosRequestConfig = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
 
 export const login = async ({
   email,
   password,
-}: Partial<UserCredentials>): Promise<LoggedUserResponse> => {
-  const requestConfig: AxiosRequestConfig = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+}: Partial<UserLoginCredentials>): Promise<LoggedUserResponse> => {
+  try {
+    const { data } = await api.post<LoggedUserResponse>(
+      `/auth`,
+      {
+        email,
+        password,
+      },
+      requestConfig,
+    );
 
-  const { data } = await api.post<LoggedUserResponse>(
-    `/auth`,
-    {
-      email,
-      password,
-    },
-    requestConfig,
-  );
+    return data;
+  } catch (err) {
+    throw new ResponseErrorHandler(err);
+  }
+};
 
-  return data;
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: Partial<UserSignupCredentials>): Promise<CreatedUserResponse> => {
+  try {
+    const { data } = await api.post<CreatedUserResponse>(
+      `/users`,
+      {
+        name,
+        email,
+        password,
+      },
+      requestConfig,
+    );
+
+    return data;
+  } catch (err) {
+    throw new ResponseErrorHandler(err);
+  }
 };
