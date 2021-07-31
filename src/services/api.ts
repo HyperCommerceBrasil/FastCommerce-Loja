520865038292d167c03b7b3b031d68250d827bef
Gabao-Farias-Builders <gabriel.farias@platformbuilders.io>
2021-07-31 18:49:28 -0300

    removed reactotron

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,7 @@
 import axios from 'axios';
-import Reactotron from 'reactotron-react-js';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-});
-
-api.interceptors.response.use(response => {
-  if (Reactotron.log)
-    Reactotron.log({
-      url: `${response.config.baseURL}${response.config.url}`,
-      data: response.data,
-    });
-  return response;
 });
 
 export default api;
