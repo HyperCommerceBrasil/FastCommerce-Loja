a71811f60a353dd64002e41199d7c31e67a5432b
Thales Morais de Almeida <thales@pop-os.localdomain>
2021-08-29 15:36:52 -0300

    Ajustado Responsividade

--- src/components/ImageCarousel/styles.ts
+++ src/components/ImageCarousel/styles.ts
@@ -1,23 +1,34 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 8px;
   display: flex;
   flex-direction: row;
+
+  @media (max-width: 900px) {
+    flex-direction: column-reverse;
+  }
 `;
 
 export const FeatureImage = styled.div`
   img {
-    max-width: 400px;
-    max-height: 400px;
+    width: 100%;
+    height: 100%;
+    max-width: 250px;
+    max-height: 250px;
     min-width: 400px;
+
+    @media (max-width: 900px) {
+      margin-bottom: 16px;
+    }
   }
 `;
 
 export const Gallery = styled.div`
+  width: 100%;
+  max-width: 200px;
   img {
-    max-width: 70px;
-    max-height: 70px;
+    max-width: 50px;
+    max-height: 50px;
     cursor: pointer;
     margin: auto;
   }
@@ -27,14 +38,24 @@ export const Gallery = styled.div`
     flex-direction: column;
     list-style: none;
 
+    @media (max-width: 900px) {
+      flex-direction: row;
+      margin-left: auto;
+    }
+
     li {
       display: flex;
       margin: 8px;
       border: 2px silver solid;
       padding: 8px;
-      height: 70px;
-      width: 70px;
+      height: 60px;
+      width: 60px;
       transition: ease-in-out 0.3s;
+
+      @media (max-width: 350px) {
+        height: 40px;
+        width: 40px;
+      }
 
       &:hover {
         border: 2px solid #cf5711;
