ccda390ddc6e4e42b0c94e6a75ba630f452a1cde
Gabao-Farias-Builders <gabriel.farias@platformbuilders.io>
2021-09-07 00:58:52 -0300

    changed enum names

--- src/utils/enums/storageKeys.ts
+++ src/utils/enums/storageKeys.ts
@@ -2,11 +2,18 @@
  * Use these keys when you need some value to be stored in local storage.
  * If there isn't any key for your data, create your key here :)
  */
-export enum STORAGE_KEYS {
+export enum LOCAL_STORAGE_KEYS {
   /**
    * Cart products (temporarily)
    */
   CART_PRODUCTS = '@FASTCOMMERCE_STORE_CART_PRODUCTS',
+}
+
+/**
+ * Use these keys when you need some value to be stored in session storage.
+ * If there isn't any key for your data, create your key here :)
+ */
+export enum SESSION_STORAGE_KEYS {
   /**
    * User object containing token login and useId
    */
